6cf92f8e340fd7c7e413ed43f87a52983045ab3f
Scienthuhsiasts <Scienthuhsiasts@github.com>
2024-06-05 02:10:00 +0800

    update_24_6_5_add_WBF

--- utils/util.py
+++ utils/util.py
@@ -6,6 +6,8 @@ import cv2
 import argparse
 import importlib
 import json
+import time
+from collections import Counter
 from PIL import Image
 import matplotlib.pyplot as plt
 import random
@@ -13,7 +15,9 @@ from torch import nn
 from pycocotools.coco import COCO
 from pycocotools.cocoeval import COCOeval
 from matplotlib.patches import Polygon, Rectangle
+from ensemble_boxes import weighted_boxes_fusion as wbf
 
+from datasets.preprocess import Transform
 
 
 def sigmoid(x):
@@ -45,6 +49,30 @@ def seed_everything(seed):
     torch.cuda.manual_seed_all(seed)
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
+
+
+
+
+
+
+def loadWeightsBySizeMatching(model:nn.Module, ckpt_path:str):
+    print('Loading weights into state dict by size matching...')
+    model_dict = model.state_dict()
+    pretrained_dict = torch.load(ckpt_path)
+    a = {}
+    for (kk, vv), (k, v) in zip(pretrained_dict.items(), model_dict.items()):
+        try:    
+            if np.shape(vv) ==  np.shape(v):
+                # print(f'(previous){kk} -> (current){k}')
+                a[k]=vv
+        except:
+            print(f'(previous){kk} mismatch (current){k}')
+    model_dict.update(a)
+    model.load_state_dict(model_dict)
+    print('Finished!')
+
+    return model
+
 
 
 
@@ -309,6 +337,198 @@ def evalCOCOmAP(GTAnnJsonPath, predAnnJsonPath):
     print(AP_table)
     # 返回mAP, AP50
     return mAPDataAvg[1], mAPDataAvg[5]
+
+
+
+
+
+
+class TTA():
+    '''Test Time Augmentation + WBF策略
+    '''
+
+    def __init__(self, tta_img_size=[[640,640], [832,832], [960,960]]):
+        self.tta_img_size = tta_img_size
+        self.tta_transform = [Transform(img_size, box_format='coco') for img_size in self.tta_img_size]        
+
+
+    def infer(self, model, image:np.array, device, T, image2color, agnostic=False, vis_heatmap=False, save_vis_path=None, half=False, tta=True):
+        flip_image = cv2.flip(image, 1)
+        # n个尺度, 每个尺度2个增强, 一共2n张图
+        tta_boxes, tta_box_scores, tta_box_classes = [], [], []
+        # 每张图像分别推理:
+        for i in range(len(self.tta_img_size)):
+            for j in range(2):
+                img = image if j==0 else flip_image
+                boxes, box_scores, box_classes = model.infer(img, self.tta_img_size[i], self.tta_transform[i], device, T, image2color, agnostic, False, save_vis_path, half=half, tta=tta)
+                if len(boxes) > 0:
+                    # 归一化(wbf只接受归一化输入)
+                    boxes[:, [0,2]] /= image.shape[1]
+                    boxes[:, [1,3]] /= image.shape[0]
+                    # 水平翻转的图像翻转回来
+                    if j == 1: 
+                        boxes[:, [0, 2]] = 1 - boxes[:, [2, 0]]
+                tta_boxes.append(boxes)
+                tta_box_scores.append(box_scores)
+                tta_box_classes.append(box_classes)
+        '''不同增强下的预测结果执行WBF'''
+        tta_boxes, tta_box_scores, tta_box_classes = wbf(tta_boxes, tta_box_scores, tta_box_classes, weights=[1]*2*len(self.tta_img_size), iou_thr=0.55, skip_box_thr=T)
+        if len(tta_boxes) == 0 : return [],[],[]
+        # 坐标再映射回原图大小
+        tta_boxes[:, [0,2]] *= image.shape[1]
+        tta_boxes[:, [1,3]] *= image.shape[0]
+        return tta_boxes, tta_box_scores, tta_box_classes.astype(int)
+
+
+
+
+
+
+
+
+
+
+
+
+
+def inferenceSingleImg(model, device, class_names, image2color, img_size, tf, img_path, save_vis_path=None, ckpt_path=None, T=0.3, agnostic=False, show_text=True, vis_heatmap=False, half=False, tta=False):
+    '''推理一张图
+        # Args:
+            - device:        cpu/cuda
+            - class_names:   每个类别的名称, list
+            - image2color:   每个类别一个颜色
+            - img_size:      固定图像大小 如[832, 832]
+            - tf:            数据预处理(基于albumentation库)
+            - img_path:      图像路径
+            - save_vis_path: 可视化图像保存路径
+            - ckpt_path:     模型权重路径
+            - T:             可视化的IoU阈值
+
+        # Returns:
+            - boxes:       网络回归的box坐标    [obj_nums, 4]
+            - box_scores:  网络预测的box置信度  [obj_nums]
+            - box_classes: 网络预测的box类别    [obj_nums]
+    '''
+    if ckpt_path != None:
+        model.load_state_dict(torch.load(ckpt_path))
+        model.eval()
+
+    image = Image.open(img_path).convert('RGB')
+    # Image 转numpy
+    image = np.array(image)
+    '''推理一张图像'''
+    if tta:
+        boxes, box_scores, box_classes = model.tta.infer(model, np.array(image), device, T, image2color, agnostic, vis_heatmap, save_vis_path, half=half)
+    else:
+        # xyxy
+        boxes, box_scores, box_classes = model.infer(np.array(image), img_size, tf, device, T, image2color, agnostic, vis_heatmap, save_vis_path, half=half)
+    #  检测出物体才继续    
+    if len(boxes) == 0: 
+        print(f'no objects in image: {img_path}.')
+        return boxes, box_scores, box_classes
+
+    '''画框'''
+    if save_vis_path!=None:
+        # PltDrawBox(image, boxes, box_classes, box_scores, save_vis_path, image2color, class_names)
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        image = OpenCVDrawBox(image, boxes, box_classes, box_scores, save_vis_path, image2color, class_names, resize_size=[2000, 2000], show_text=show_text)
+        cv2.imwrite(save_vis_path, image)
+        # 统计检测出的类别和数量
+        detect_cls = dict(Counter(box_classes))
+        detect_name = {}
+        for key, val in detect_cls.items():
+            detect_name[class_names[key]] = val
+        print(f'detect result: {detect_name}')
+    return boxes, box_scores, box_classes
+
+
+
+
+
+
+
+
+
+
+def inferenceVideo(model, device, class_names, image2color, img_size, tf, video_path, save_vis_path=None, ckpt_path=None, T=0.3, agnostic=False, show_text=True, half=False):
+    '''推理一段视频
+        # Args:
+            - device:        cpu/cuda
+            - class_names:   每个类别的名称, list
+            - image2color:   每个类别一个颜色
+            - img_size:      固定图像大小 如[832, 832]
+            - tf:            数据预处理(基于albumentation库)
+            - video_pat:     视频路径
+            - save_vis_path: 可视化图像保存路径
+            - ckpt_path:     模型权重路径
+            - T:             可视化的IoU阈值
+
+        # Returns:
+            - boxes:       网络回归的box坐标    [obj_nums, 4]
+            - box_scores:  网络预测的box置信度  [obj_nums]
+            - box_classes: 网络预测的box类别    [obj_nums]
+    '''
+    if ckpt_path != None:
+        model.load_state_dict(torch.load(ckpt_path))
+        # model = loadWeightsBySizeMatching(model, ckpt_path)
+        model.eval()
+    # 创建视频捕获对象
+    cap = cv2.VideoCapture(video_path)
+    # 获取视频的基本信息
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    # 定义视频编码器和创建 VideoWriter 对象
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')  # 或者 'XVID' 'mp4v'
+    out = cv2.VideoWriter(save_vis_path, fourcc, fps, (width, height))
+    # 检查视频是否正确打开
+    if not cap.isOpened():
+        print("Error: Could not open video.")
+        exit()
+
+    # 逐帧读取视频
+    cnt_frame =  1
+    start_time = time.time()
+    while True:
+        ret, frame = cap.read()  # ret是一个布尔值，frame是每一帧的图像
+        if not ret:
+            print("Reached end of video or failed to read frame.")
+            break
+
+        '''此处为推理'''
+        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+        boxes, box_scores, box_classes = model.infer(frame, model.img_size, tf, device, T, agnostic, half=half)
+        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+        #  检测出物体才继续    
+        if len(boxes) != 0: 
+            '''画框'''
+            frame = OpenCVDrawBox(frame, boxes, box_classes, box_scores, None, image2color, class_names, resize_size=[2000, 2000], show_text=show_text)
+            # 统计检测出的类别和数量
+            detect_cls = dict(Counter(box_classes))
+            detect_name = {}
+            for key, val in detect_cls.items():
+                detect_name[class_names[key]] = val
+        # 写入处理后的帧到新视频
+        out.write(frame)
+        print(f'process frame {frame.shape}: [{cnt_frame}/{total_frames}] | {detect_name}')
+        cnt_frame += 1
+    end_time = time.time()
+    print(f"total_time: {end_time - start_time}(s) | fps: {cnt_frame / (end_time - start_time)}")
+    # 释放视频捕获对象和视频写入对象，销毁所有OpenCV窗口
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
+
+
+
+
+
+
+
+
+
+
 
 
 
